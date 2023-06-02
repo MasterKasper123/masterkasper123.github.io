223c4dd96611d1525275f11503b616af4bf8dd82
MasterKasper123 <104412818+MasterKasper123@users.noreply.github.com>
2023-06-02 08:53:05 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -95,7 +95,7 @@ const textNodes = [
       },
       {
         text: 'Explore the sinister banana forest',
-        nextText: 13
+        nextText: 17
       }
     ]
   },
@@ -129,7 +129,7 @@ const textNodes = [
       },
       {
         text: 'Explore the sinister banana-forest',
-        nextText: 13
+        nextText: 18
       }
     ]
   },
@@ -145,46 +145,41 @@ const textNodes = [
         text: 'Explore the chest',
         requiredState: (currentState) => currentState.sword,
         nextText: 15
-      },
-      {
-        text: 'Hide behind your shield',
-        requiredState: (currentState) => currentState.shield,
-        nextText: 10
-      },
-      {
-        text: 'Throw the rotten banana at it',
-        requiredState: (currentState) => currentState.rottenBanana,
-        nextText: 11
       }
     ]
   },
   {
     id: 8,
-    text: 'You walk into the room, amazed by all the precious and beautiful diamonds. This chamber is stunning. How can all these diamonds have been missed by previous treasure hunters, you wonder. And thats when you see the hole. In the middle of the room there is a big hole in the floor, it looks a little bit like a well. You hear some mysterious sounds coming out of the dark hole, so you walk closer. You stand right by the pit and look down into it. You can not see the bottom because it is curved, but you can see a green light coming up from somewhere down there.',
+    text: "You walk into the room, amazed by all the precious and beautiful diamonds. This chamber is stunning. How can all these diamonds have been missed by previous treasure hunters, you wonder. And that's when you see the hole. In the middle of the room there is a big hole in the floor, it looks a little bit like the type of hole you dig when you're making a well. You hear some mysterious sounds coming out of the dark hole, so you walk closer. You stand right by the pit and look down into it. You can't see the bottom of the hole because it has a slight curve, but you can see a green light shining up from somewhere down there.",
     options: [
       {
         text: 'Jump into the hole',
         nextText: 13
       },
       {
-        text: 'Run away',
+        text: 'Slowly back away from the hole',
         nextText: 14
       }
     ]
   },
   {
     id: 9,
-    text: 'You foolishly thought this monster could be slain with a single sword.',
+    text: "As you take take out your sword to defend yourself, the monster immediately tries to strike the sword from your hands, but the sword is really sharp and your reflexes are quick, so you manage to cut one of the monsters arms in half. The monster screams and because of that first hit you manage to defeat the terrible beast of evil. Now that you've seen what kind of creatures that lurk in this temple the only thing you wanna do is leave. Unfortunately, you now realise that there is no exit to this room and that the noise you heard before was the opening beeing sealed by a giant boulder of granite. You try to push it out of the way, but you can't even move it a centimeter. The only other way leading out of this room is through the hole in the floor where there must be something that is emitting the mysterious green light.",
     options: [
       {
-        text: 'Restart',
-        nextText: -1
+        text: 'Throw the rotten banana into the hole',
+        requiredState: (currentState) => currentState.rottenBanana,
+        nextText: 11
+      },
+      {
+        text: "Jump into the hole",
+        nextText: 13
       }
     ]
   },
   {
     id: 10,
-    text: 'The monster laughed as you hid behind your shield and ate you.',
+    text: "You try to hide behind your shield, but the monster easily grabs it out of your hand, and with no defence left, this is where your journey ends.",
     options: [
       {
         text: 'Restart',
@@ -194,11 +189,11 @@ const textNodes = [
   },
   {
     id: 11,
-    text: 'You threw your jar of goo at the monster and it exploded. After the dust settled you saw the monster was destroyed. Seeing your victory you decide to claim this castle as your and live out the rest of your days there.',
+    text: "You drop the banana into the hole, and it slides down the side of the pit. After ten meters you can't see it anymore because of the hole is so curved, but you still hear it slide rapidly down the hole. You hear it hit the bottom, and shortly after that you hear A LOT of buzzing and squeaking. It sounds like you've just woken up an enormous tribe of insects. After a while of nervous waiting, the sounds slowly start to stop and even the green light starts to go out. After about five minutes all sounds have stopped and the green light is gone. Now the only thing left to do is to jump down into the hole.",
     options: [
       {
-        text: 'Congratulations. Play Again.',
-        nextText: -1
+        text: 'Jump into the hole',
+        nextText: 15
       }
     ]
   },
@@ -227,18 +222,104 @@ const textNodes = [
         requiredState: (currentState) => currentState.shield,
         setState: { shield: false},
         nextText: 6
-      },
-      {
+      }
+    ]
+  },
+  {
     id: 13,
-    text: "You fall down about twenty meters and the only thing stopping you from dying from fall damage is te fact that the pit isn't straight. You get slung into the walls many times as you fall down into the home of thousands of black insects that give away a green light from a sting on thier back. You're already hurt from the fall and the bugs easily eat you and build a nest out of you. You stupidly decided to jump into a deep and evil hole that looks very dangerous inside of an ancient temple all alone. What did you expect? ",
+    text: "You fall down about twenty meters and the only thing stopping you from dying from fall damage is the fact that the fall isn't straight. You get slung into the walls many times as you fall down into the nest of thousands of surprised orange-brown colored insects the size of a child's fist. You're already hurt from the fall and the bugs are way to many for you to handle. Is this how your adventure finally ends, overwhelmed by buzzing, banana intolerant bugs that look like they're taken straight out of a horror movie after you decided to jump into a deep and evil-looking hole inside of an ancient temple all alone?",
     options: [
       {
-        text: 'Congratulations. Play Again.',
+        text: "You get eaten up by bugs, restart.",
         nextText: -1
       }
     ]
   },
+  {
+    id: 14,
+    text: "You slowly start to back away from the hole, step by step. Watching the hole carefully as if it's about to explode, you walk backwards towards the exit of the room. When you're almost out of the room you hear a noise behind you. You turn around and you scream out loud as you see the entity that made the sound. In front of you, blocking the exit of the room, is the most horrible and scary monster ever imaginable. At least 2.5 meters tall and with long, slim and partly rotten arms and legs. It's a miracle that you don't immediately faint when you see the furious beast preparing for an attack.",
+    options: [
+      {
+        text: "Jump into the hole",
+        nextText: 13
+      },
+      {
+        text: 'Attack the monster with your sword',
+        requiredState: (currentState) => currentState.sword,
+        nextText: 9
+      },
+      {
+        text: 'Hide behind your shield',
+        requiredState: (currentState) => currentState.shield,
+        nextText: 10
+      }
+    ]
+  },
+  {
+    id: 15,
+    text: "You slide down the side of the hole, just like your banana did, and when you hit the bottom, you are standing in the middle of the nest of thousands of deadly big insects. However, they all seem to be sleeping. You see the banana you threw in earlier and putting together two and two, you come to the conclusion that the bad smell of the banana made the bugs fall asleep. You have no idea of how long they will sleep so you quickly begin to search for an exit, logically these bugs must have some kind of way to get outside to get food. After a few minutes of searching you finally find the exit. It's a circular tunnel and it's only about one meter in diameter. When you look into it you can't see the end of it. Although it would be very VERY scary to crawl through it, you probably would make it, IF that is, is actually leads to the surface outside of the temple.",
+    options: [
+      {
+        text: "Crawl through the tunnel and hope for the best",
+        nextText: 16
+      }
+    ]
+  },
+  {
+    id: 16,
+    text: "You start to crawl through the tight tunnel, and it's the worst experience of your entire life. It feels like it's never gonna end and it feels like you're gonna get stuck and lie in this tunnel forever. Luckily, that's not the case and after about half an hour of crawling you finally reach the end of the tunnel. The tunnel ends in a nearby cave and you're finally free from this terrible ancient building. As you breathe in the fresh air you swear to never enter the tomb ever again. You go to the village and tell everyone your story and you become thier hero. The monster that you defeater had been threatening them for years and they get really happy when you tell them that it's dead. And then you live happily for the rest of your life.",
+    options: [
+      {
+        text: 'Congratulations for winning the game! Play again?',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 17,
+    text: "You enter the forest although you feel very tired and would like to sleep. You have a very bad feeling about this. You follow the road about 500 meters into the forest. The forest is very dense and all the big banana trees make it very dark this far into the forest. You start to hear weird sounds around you. You think it's just imagination, so you continue on deeper into the forest. When you are about one mile into the forest you hear quick footsteps behind you. You turn around, scared, but you can't see anything in the dark. The footsteps continue, faster and faster, closer and closer. You get scared and try to run away from the thing that's running towards you, but that's when you run straight into a big banana tree. You faint, and get killed by the weird banana man that hates all humans.",
+    options: [
+      {
+        text: 'You lost, restart?',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 18,
+    text: "You enter the forest, and the first thing you nitice is the bad smell. But since you're full of energy from having such a good sleep last night, you don't care about the smell. You walk further into the forest with confidence in your legs. When you're about one mile in, you see a dark figure about 50 meters away on the road. You stop walking, and start to observe the it closer. It looks like a ghost of some sort. While you're watching the banana deamon, it suddenly turns around and spots you. It lets out a horrifying scream, and start to walk towards you with a scythe in it's hands.",
+    options: [
+      {
+        text: 'RUN BACK OUT OF THE FOREST',
+        nextText: 19
+      },
+      {
+        text: "Take the risk to quickly grab a banana from one of the trees before you run out of the forest",
+        setState: { rottenBanana: true},
+        nextText: 20
+      }
+    ]
+  },
+  {
+    id: 19,
+    text: "You run as fast as possible away from the scary phantom and you manage to outrun it. When you're outside of the forest it can't reach you anymore. Now that you have been in the forest, you're excited to explore the desert temple, or is it catacombs? ",
+    options: [
+      {
+        text: 'Enter the temple',
+        nextText: 7
+      }
+    ]
+  },
+  {
+    id: 20,
+    text: "You actually manage to grab a banana before you flee as fast as possible away from the scary phantom and you manage to outrun it. When you're outside of the forest it can't reach you anymore. Now that you have been in the forest, you're excited to explore the desert temple, or is it catacombs? ",
+    options: [
+      {
+        text: 'Enter the temple',
+        nextText: 7
+      }
     ]
   }
+
 ]
 startGame()
